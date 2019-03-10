d0b0fa2464353f3717d7d974b8d2778d190135ee
Flound <floundkeeper@mail.ru>
2019-03-11 00:56:36 +0300

    Regular updates. Add file creator for log

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,54 @@
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        try(FileReader reader = new FileReader("./Data/Input.txt"))
+        {
+            // читаем посимвольно
+            int c;
+            String num = "";
+            while((c = reader.read()) != 10 && c != -1) {
+
+                num = num + (char) c;
+            }
+
+            num = "";
+            while((c = reader.read()) != 10 && c != -1) {
+
+                num = num + (char) c;
+            }
+
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        /*
         // Инициализация объекта date
         Date dateNow = new Date();
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("ddMMyyyy_HHmmss");
 
         try(FileWriter writer = new FileWriter("./Log/" + formatForDateNow.format(dateNow) + ".txt", false))
         {
-            // запись всей строки
-            String text = "Hello Gold!";    // Инициализация строки которая будет записана в файл
-            writer.write(text);             //Запись строки в открытый файл
-            writer.flush();
+        // запись всей строки
+        String text = "Hello Gold!";    // Инициализация строки которая будет записана в файл
+        writer.write(text);             //Запись строки в открытый файл
+        writer.flush();
         }
         catch(IOException ex){
-            System.out.println(ex.getMessage());
+        System.out.println(ex.getMessage());
         }
+        */
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,24 @@
 public class Methods {
+    static double stringToNumber (String input, int num_sys){
+
+        String left = input.charAt(0) == '-' || input.charAt(0) == '+' ? input.substring(1, input.indexOf(',')) : input.substring(0, input.indexOf(','));
+        String right = input.substring(input.indexOf(',') + 1);
+
+        System.out.println(left + " " + right);
+
+        double integer_part = 0;
+        double fractional_part = 0;
+
+        for (int i = 0; i < left.length(); i++)
+        {
+            integer_part += Math.pow(num_sys, i) * Character.getNumericValue(left.charAt(left.length() - i - 1));
+        }
+        
+        for (int i = 0; right.length() > 0 && (right.charAt(right.length() - 1) == ',' ? i < right.length() - 1 : i < right.length()); i++)
+        {
+            fractional_part += Math.pow(num_sys, (i + 1) * -1) * Character.getNumericValue(right.charAt(i));
+        }
+
+        return input.charAt(0) == '-' ? (integer_part + fractional_part) * -1 : integer_part + fractional_part;
+    }
 }
