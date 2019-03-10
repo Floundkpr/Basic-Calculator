98886bec5b2c0bcde2b740dcb6b931c0a6a6c48e
Flound <floundkeeper@mail.ru>
2019-03-10 20:01:31 +0300

    Regular updates. Add file creator for log

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,38 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Инициализация объекта date
         Date dateNow = new Date();
-
-        // Вывод текущей даты и времени с использованием toString()
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("ddMMyyyy_HHmmss");
 
-        try(FileWriter writer = new FileWriter("./SomeDir/" + formatForDateNow.format(dateNow) + ".txt", false))
+        try(FileWriter writer = new FileWriter("./Log/" + formatForDateNow.format(dateNow) + ".txt", false))
         {
             // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-
+            String text = "Hello Gold!";    // Инициализация строки которая будет записана в файл
+            writer.write(text);             //Запись строки в открытый файл
             writer.flush();
         }
         catch(IOException ex){
 
             System.out.println(ex.getMessage());
         }
-
-        System.out.println("Текущая дата " + formatForDateNow.format(dateNow));
     }
 }
