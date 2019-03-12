f4f4c5e76c7950bd4ae9373d1af15a130c609e03
Flound <floundkeeper@mail.ru>
2019-03-12 19:36:16 +0300

    Final push. Project is ready.

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,103 @@
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        try(FileReader reader = new FileReader("./Data/Input.txt"))
-        {
-            // читаем посимвольно
-            int c;
-            String num = "";
-            while((c = reader.read()) != 10 && c != -1) {
+        Scanner scanner = new Scanner(System.in);
+        String[] numbers = new String[2];
+        int num_sys = 10;
+        boolean f;
+        StringBuilder protocol = new StringBuilder();
+        double result = 0;
 
-                num = num + (char) c;
+
+        do {
+            f = false;
+            System.out.println("Выберите способ считывания данных:\n 1. Ввод с клавиатуры\n 2. Считать из файла Input.txt в папке Data\n");
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("Введите первое число  ");
+                    scanner.nextLine();
+                    numbers[0] = scanner.nextLine();
+                    System.out.println("Введите второе число  ");
+                    numbers[1] = scanner.nextLine();
+                    protocol.append("С консоли считаны числа ").append(numbers[0]).append(" и ").append(numbers[1]).append("\n");
+                    break;
+                case 2:
+                    numbers = Methods.readFromFile();
+                    protocol.append("С файла считаны числа ").append(numbers[0]).append(" и ").append(numbers[1]).append("\n");
+                    break;
+                default:
+                    System.out.println("Введены неверные данные");
+                    f = true;
+                    break;
             }
+        } while (f);
 
-            num = "";
-            while((c = reader.read()) != 10 && c != -1) {
-
-                num = num + (char) c;
+        do {
+            f = false;
+            System.out.println("Выберите систему счисления:\n 1. Двоичная\n 2. Восмеричная\n 3. Десятичная\n 4. Шеснадцатиричная\n");
+            switch (scanner.nextInt()) {
+                case 1:
+                    num_sys = 2;
+                    protocol.append("Выбрана двоичная система счисления\n");
+                    break;
+                case 2:
+                    num_sys = 8;
+                    protocol.append("Выбрана восмеричная система счисления\n");
+                    break;
+                case 3:
+                    num_sys = 10;
+                    protocol.append("Выбрана десятичная система счисления\n");
+                    break;
+                case 4:
+                    num_sys = 16;
+                    protocol.append("Выбрана шеснадцатиричная система счисления\n");
+                    break;
+                default:
+                    System.out.println("Введены неверные данные");
+                    f = true;
+                    break;
             }
+        } while (f);
 
-        }
-        catch(IOException ex){
+        System.out.println("Введите точность вычислений (число знаков после запятой)");
+        int numbersOfFraction = scanner.nextInt();
 
-            System.out.println(ex.getMessage());
-        }
+        double num1 = Methods.stringToNumber(numbers[0], num_sys);
+        double num2 = Methods.stringToNumber(numbers[1], num_sys);
 
+        do {
+            f = false;
+            System.out.println("Выберите действие:\n 1. Сложение (+)\n 2. Вычитание (-)\n 3. Умножение (*)\n 4. Деление (/)\n");
+            switch (scanner.nextInt()) {
+                case 1:
+                    result = num1 + num2;
+                    protocol.append("Выбрано сложение\n");
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    protocol.append("Выбрано вычитание\n");
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    protocol.append("Выбрано умножение\n");
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    protocol.append("Выбрано деление\n");
+                    break;
+                default:
+                    System.out.println("Введены неверные данные");
+                    f = true;
+                    break;
+            }
+        } while (f);
 
+        System.out.println("Результат вычисления: " + Methods.numberToString(result, num_sys, numbersOfFraction));
+        protocol.append("Результат вычисления: ").append(Methods.numberToString(result, num_sys, numbersOfFraction)).append("\n");
+        protocol.append("Программа завершила свою работу");
 
-
-        /*
-        // Инициализация объекта date
-        Date dateNow = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("ddMMyyyy_HHmmss");
-
-        try(FileWriter writer = new FileWriter("./Log/" + formatForDateNow.format(dateNow) + ".txt", false))
-        {
-        // запись всей строки
-        String text = "Hello Gold!";    // Инициализация строки которая будет записана в файл
-        writer.write(text);             //Запись строки в открытый файл
-        writer.flush();
-        }
-        catch(IOException ex){
-        System.out.println(ex.getMessage());
-        }
-        */
+        Methods.toProtocol(protocol.toString().split("\n"));
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,10 +1,38 @@
-public class Methods {
+import java.io.FileReader;
+import java.io.FileWriter;
+
+class Methods {
+
+    static String[] readFromFile (){
+        String[] out = {"",""};
+        try(FileReader reader = new FileReader("./Data/Input.txt"))
+        {
+            int chr;
+            while((chr = reader.read()) != 13 && chr != -1) {
+
+                out[0] = out[0] + (char) chr;
+
+            }
+            chr = reader.read();
+            while((chr = reader.read()) != 13 && chr != -1) {
+
+                out[1] = out[1] + (char) chr;
+            }
+            return out;
+        }
+        catch(Exception ex) {
+            out[0] = "0";
+            out[1] = "0";
+            System.out.println(ex.getMessage());
+            return out;
+        }
+    }
+
     static double stringToNumber (String input, int num_sys){
 
+        input = input + ',';
         String left = input.charAt(0) == '-' || input.charAt(0) == '+' ? input.substring(1, input.indexOf(',')) : input.substring(0, input.indexOf(','));
         String right = input.substring(input.indexOf(',') + 1);
-
-        System.out.println(left + " " + right);
 
         double integer_part = 0;
         double fractional_part = 0;
@@ -21,4 +49,93 @@ public class Methods {
 
         return input.charAt(0) == '-' ? (integer_part + fractional_part) * -1 : integer_part + fractional_part;
     }
+
+    static StringBuilder numberToString(double num, int num_sys, int numbersOfFraction) {
+        StringBuilder result = new StringBuilder();
+
+        int ceil = (int) Math.floor(num);
+        double fraction = num - ceil;
+
+        do {
+            if (num_sys > 10) {
+                switch (ceil % num_sys) {
+                    case 10:
+                        result.append("A");
+                        break;
+                    case 11:
+                        result.append("B");
+                        break;
+                    case 12:
+                        result.append("C");
+                        break;
+                    case 13:
+                        result.append("D");
+                        break;
+                    case 14:
+                        result.append("E");
+                        break;
+                    case 15:
+                        result.append("F");
+                        break;
+                    default:
+                        result.append(ceil % num_sys);
+                        break;
+                }
+            } else result.append(ceil % num_sys);
+            ceil = (int) Math.floor((double) ceil / num_sys);
+        } while (ceil != 0);
+
+        result.reverse().append(",");
+
+        for (int i = 0; i < numbersOfFraction; i++){
+            if (num_sys > 10) {
+                switch ((int)Math.floor(fraction * num_sys)) {
+                    case 10:
+                        result.append("A");
+                        break;
+                    case 11:
+                        result.append("B");
+                        break;
+                    case 12:
+                        result.append("C");
+                        break;
+                    case 13:
+                        result.append("D");
+                        break;
+                    case 14:
+                        result.append("E");
+                        break;
+                    case 15:
+                        result.append("F");
+                        break;
+                    default:
+                        result.append((int)Math.floor(fraction * num_sys));
+                        break;
+                }
+            } else {
+                 result.append((int)Math.floor(fraction * num_sys));
+            }
+            fraction = fraction * num_sys - Math.floor(fraction * num_sys);
+        }
+
+        return result;
+    }
+
+    static void toProtocol(String[] protocol){
+        try(FileWriter writer = new FileWriter("./Data/Protocol.txt", false))
+        {
+            for (String s : protocol) {
+                writer.write(s);
+                writer.append('\n');
+            }
+                         //Запись строки в открытый файл
+            writer.flush();
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+
 }
